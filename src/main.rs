9a92db2999240074c3f37982da2d6c8cd22d0478
danielsan901998 <danielsan901998@gmail.com>
2024-09-18 20:16:46 +0200

    recursive state

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,15 @@ struct HtmlWalker<'a> {
     handle: &'a Handle,
     ignore_elements: HashSet<String>,
 }
+#[derive(Clone, Copy)]
+enum State {
+    Search,
+    P,
+    Pre,
+    Ol,
+    Ul,
+    Table,
+}
 
 impl<'a> HtmlWalker<'a> {
     fn new(handle: &'a Handle) -> Self {
@@ -25,24 +34,34 @@ impl<'a> HtmlWalker<'a> {
     }
 
     fn walk(&mut self) {
-        self.walk_inner(self.handle);
+        self.visit(self.handle, State::Search);
     }
 
-    fn walk_children(&mut self, node: &Handle) {
+    fn walk_children(&mut self, node: &Handle, e: State) {
         for child in node.children.borrow().iter() {
-            self.walk_inner(child);
+            self.visit(child, e);
         }
     }
 
-    fn walk_inner(&mut self, node: &Handle) {
+    fn visit(&mut self, node: &Handle, e: State) {
         match node.data {
-            NodeData::Document => self.walk_children(node),
+            NodeData::Document => self.walk_children(node, e),
             NodeData::Text { ref contents } => {
-                if self.found_article {
-                    let text = contents.borrow().replace('\n', " ");
-                    let text = text.trim();
-                    if !text.is_empty() {
-                        println!("{}", text);
+                match e {
+                    State::Search =>{
+                        if self.found_article {
+                            let text = contents.borrow().replace('\n', " ");
+                            let text = text.trim();
+                            if !text.is_empty() {
+                                println!("{}", text);
+                            }
+                        }
+                    }
+                    State::Pre => {
+                        print!("{}", contents.borrow());
+                    }
+                    _ =>{
+                        print!("{}", contents.borrow().replace('\n', " "));
                     }
                 }
             }
@@ -56,7 +75,7 @@ impl<'a> HtmlWalker<'a> {
                     return;
                 }
 
-                self.handle_element(name, attrs, node);
+                self.handle_element(name, attrs, node, e);
             }
             _ => {}
         }
@@ -66,13 +85,14 @@ impl<'a> HtmlWalker<'a> {
         name: &str,
         attrs: &RefCell<Vec<html5ever::Attribute>>,
         node: &Handle,
+        state: State,
     ) {
         if is_article(name, attrs) {
             self.found_article = true;
-            self.walk_children(node);
+            self.walk_children(node, state);
         } else if self.found_article {
             match name {
-                "p" => self.print_element(node),
+                "p" => self.print_element(node, State::P),
                 "pre" => {
                     self.print_pre(node);
                     println!();
@@ -93,63 +113,61 @@ impl<'a> HtmlWalker<'a> {
                 }
                 "ul" => self.print_ul(node),
                 "table" => self.print_table(node),
-                _ if name.starts_with('h') && name.len() == 2 => self.print_element(node),
-                _ => self.walk_children(node),
+                _ if name.starts_with('h') && name.len() == 2 => self.print_element(node, State::P),
+                _ => self.walk_children(node, state),
             }
         } else {
-            self.walk_children(node);
+            self.walk_children(node, state);
         }
     }
-    fn print_text(&self, handle: &Handle) {
-        let node = handle;
-        for child in node.children.borrow().iter() {
+    fn print_text(&mut self, handle: &Handle, e: State) {
+        for child in handle.children.borrow().iter() {
             match child.data {
                 NodeData::Text { ref contents } => {
                     print!("{}", contents.borrow().replace('\n', " "));
                 }
 
-                NodeData::Element { ref name, .. } => {
+                NodeData::Element { ref name,
+                ref attrs,
+                .. } => {
                     let name = std::str::from_utf8(name.local.as_bytes()).unwrap();
                     if self.ignore_elements.contains(name) {
                         return;
-                    } else if name == "br" {
-                        println!();
-                    } else {
-                        self.print_text(child);
                     }
+                    self.handle_element(name, attrs, child, e);
                 }
                 _ => {}
             }
         }
     }
 
-    fn print_element(&self, handle: &Handle) {
-        self.print_text(handle);
+    fn print_element(&mut self, handle: &Handle, e: State) {
+        self.print_text(handle, e);
         println!();
     }
-    fn print_pre(&self, handle: &Handle) {
-        let node = handle;
-        for child in node.children.borrow().iter() {
+    fn print_pre(&mut self, handle: &Handle) {
+        for child in handle.children.borrow().iter() {
             match child.data {
                 NodeData::Text { ref contents } => {
                     print!("{}", contents.borrow());
                 }
 
-                NodeData::Element { ref name, .. } => {
+                NodeData::Element { ref name,
+                ref attrs,
+                .. } => {
                     let string = std::str::from_utf8(name.local.as_bytes()).unwrap();
                     if string == "br" {
                         println!();
                     } else {
-                        self.print_pre(child);
+                        self.handle_element(string, attrs, child, State::Pre);
                     }
                 }
                 _ => {}
             }
         }
     }
-    fn print_table(&self, handle: &Handle) {
-        let node = handle;
-        for child in node.children.borrow().iter() {
+    fn print_table(&mut self, handle: &Handle) {
+        for child in handle.children.borrow().iter() {
             if let NodeData::Element { ref name, .. } = child.data {
                 let string = std::str::from_utf8(name.local.as_bytes()).unwrap();
                 if string == "tr" {
@@ -157,32 +175,32 @@ impl<'a> HtmlWalker<'a> {
                     println!();
                 } else if string == "th" || string == "td" {
                     print!("\t");
-                    self.print_text(child);
+                    self.print_text(child, State::Table);
                 } else {
                     self.print_table(child);
                 }
             }
         }
     }
-    fn print_ul(&self, handle: &Handle) {
+    fn print_ul(&mut self, handle: &Handle) {
         for child in handle.children.borrow().iter() {
             if let NodeData::Element { ref name, .. } = child.data {
                 let string = std::str::from_utf8(name.local.as_bytes()).unwrap();
                 if string == "li" {
                     print!("• ");
-                    self.print_element(child);
+                    self.print_element(child, State::Ul);
                 }
             }
         }
     }
-    fn print_ol(&self, handle: &Handle, start: i32, reversed: bool) {
+    fn print_ol(&mut self, handle: &Handle, start: i32, reversed: bool) {
         let mut pos = start;
         for child in handle.children.borrow().iter() {
             if let NodeData::Element { ref name, .. } = child.data {
                 let string = std::str::from_utf8(name.local.as_bytes()).unwrap();
                 if string == "li" {
                     print!("{}. ", pos);
-                    self.print_element(child);
+                    self.print_element(child, State::Ol);
                     if reversed {
                         pos -= 1;
                     } else {
